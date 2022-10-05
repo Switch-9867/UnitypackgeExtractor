5588abbb920de3088ce09ccaa44b07ace310001f
FantasyDR <fantasydr.gg@gmail.com>
2022-10-05 14:22:24 +0900

    Fix the multi-thread issues if extract large package. Fix the file name issues and properly print the logs.

--- Program.cs
+++ Program.cs
@@ -10,219 +10,275 @@ using System.Threading.Tasks;
 
 namespace UnitypackgeExtractor
 {
-	internal class Program
-	{
-		private static string UnitypackagePath;
-		private static string UserTempFolder;
-		private static string OutputDirectory;
-		static long ThreadWork = 0;
+    internal class Program
+    {
+        private static object global = new object(); // to let log output properly
+        private static string UnitypackagePath;
+        private static string UserTempFolder;
+        private static string OutputDirectory;
+        static long ThreadWork = 0;
 
-		static bool outputMetaFiles = false;
-		static bool outputPreviewFiles = true;
+        static bool outputMetaFiles = false;
+        static bool outputPreviewFiles = true;
 
-		static void Main(string[] args)
-		{
+        static void Main(string[] args)
+        {
+            if (args.Contains("-help"))
+            {
+                OutputHelp();
+                return;
+            }
 
-			if (args.Contains("-help"))
-			{
-				OutputHelp();
-				return;
-			}
+            outputMetaFiles = !args.Contains("-nometa");
+            outputPreviewFiles = args.Contains("-outputPreview");
+            bool waitBeforeExit = args.Contains("-wait");
 
-			if (args.Contains("-outputMeta"))
-			{
-				outputMetaFiles = true;
-			}
+            if (args.Length < 1 || args[0] == null || args[0].Length < 1 || !File.Exists(args[0]) || !Path.HasExtension(".unitypackage"))
+            {
+                UnitypackagePath = GetUnityPackage();
+                waitBeforeExit = true; // force wait if it is launched by drag-drop mode
+            }
+            else
+            {
+                UnitypackagePath = args[0].Trim('"'); // handle the path with 'space' properly
+            }
 
-			if (args.Contains("-noPreview"))
-			{
-				outputPreviewFiles = false;
-			}
+            if (UnitypackagePath.Length < 2)
+            {
+                Console.WriteLine("[ERROR] Empty Unitypackage Path: " + UnitypackagePath);
+                Console.ReadKey();
+                return;
+            }
 
-			if (args.Length < 1 || args[0] == null || args[0].Length < 1 || !File.Exists(args[0]) || !Path.HasExtension(".unitypackage"))
-			{
-				UnitypackagePath = GetUnityPackage();
-				Console.WriteLine("Extracting...");
-			}
-			else
-			{
-				UnitypackagePath = args[0];
-			}
+            UserTempFolder = Path.GetTempPath();
+            if (UserTempFolder.Length < 1)
+            {
+                Console.WriteLine("[ERROR] Empty TempFolder Path" + UserTempFolder);
+                Console.ReadKey();
+                return;
+            }
 
-			if (UnitypackagePath.Length < 2)
-			{
-				Console.WriteLine("UnitypackagePath Error");
-				Console.ReadKey();
-				return;
-			}
+            OutputDirectory = GenreateOutputDirectory();
 
-			UserTempFolder = Path.GetTempPath();
-			if (UserTempFolder.Length < 1)
-			{
-				Console.WriteLine("TempFolder Error");
-				Console.ReadKey();
-				return;
-			}
+            string tempFolder = Path.Combine(UserTempFolder, "UnityPackage." + Guid.NewGuid().ToString());
 
-			OutputDirectory = GenreateOutputDirectory();
+            Console.WriteLine("Extracting '" + UnitypackagePath + "' to temp folder: " + tempFolder);
 
-			FileStream ups = File.OpenRead(UnitypackagePath);
+            FileStream ups = File.OpenRead(UnitypackagePath);
+            try
+            {
+                ExtractTGZ(tempFolder, ups);
 
-			string tempFolder = ExtractTGZ(ups);
+                string[] folders = Directory.GetDirectories(tempFolder);
 
-			IEnumerable<string> folders = Directory.EnumerateDirectories(tempFolder);
+                if (folders.Count() < 1)
+                {
+                    Console.WriteLine("[ERROR] No data extracted from unity package.");
+                    Console.ReadKey();
+                    return;
+                }
 
-			if (folders.Count() < 1)
-			{
-				Console.WriteLine("Directory Error");
-				Console.ReadKey();
-				return;
-			}
+                Console.WriteLine($"Extracting {folders.Count()} assets to: {OutputDirectory}");
 
-			Console.WriteLine($"Extracting {folders.Count()} assets to: {OutputDirectory}");
+                var errors = new List<string>();
+                var maxThread = Environment.ProcessorCount;
+                var threads = new Thread[maxThread];
+                for (int i = 0; i < maxThread; i++)
+                {
+                    Thread t = new Thread(new ThreadStart(() =>
+                    {
+                        while (true)
+                        {
+                            string folder = null;
+                            lock (folders)
+                            {
+                                if (ThreadWork >= folders.Length)
+                                    return;
 
-			foreach (string folder in folders)
-			{
-				Thread t = new Thread(new ThreadStart(() =>
-				{
-					ExtractFile(folder, OutputDirectory);
-				}));
-				t.Start();
-			}
+                                folder = folders[ThreadWork++];
+                            }
 
-			while(ThreadWork < folders.Count())
-			{
-				//Console.WriteLine($"Extracting file {ThreadWork} of {folders.Count()}");
-				Thread.Sleep(100);
-			}
-			Console.WriteLine("Finished!");
+                            try
+                            {
+                                string filename = ExtractFile(folder);
+                                lock (global)
+                                {
+                                    Console.WriteLine("Extracted file '" + filename + "' from " + Path.GetFileName(folder));
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                lock (errors)
+                                {
+                                    errors.Add("[ERROR] Failed to extract file from " + Path.GetFileName(folder) + "\n" + e.Message);
+                                }
+                            }
+                        }
+                    }));
+                    t.Start();
+                    threads[i] = t;
+                }
 
-			Console.WriteLine("Cleaning up...");
-			Console.WriteLine($"Deleting {tempFolder}");
-			Directory.Delete(tempFolder, true);
-			Console.WriteLine("Done!");
+                // properly wait all threads to finish then print the logs
+                foreach (var thread in threads)
+                {
+                    thread.Join();
+                }
 
-			Console.WriteLine("Press any key to close.");
-			Console.ReadKey();
-		}
+                // print all errors in the end
+                foreach (var err in errors)
+                    Console.WriteLine(err);
 
-		private static void OutputHelp()
-		{
-			Console.WriteLine("Usage: \"extract.exe [unitypackage file] [options]\"");
-			Console.WriteLine("Use -outputMeta to output unity's meta files");
-			Console.WriteLine("Use -noPreview to skip unity's preview files");
-		}
+                Console.WriteLine("Finished!");
 
-		private static string GenreateOutputDirectory()
-		{
-			return Path.GetFullPath(Path.Combine(UnitypackagePath, @"..\"));
-		}
+                if(waitBeforeExit)
+                {
+                    Console.WriteLine($"Press any key to delete the {tempFolder}...");
+                    Console.ReadKey();
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Cleaning up...");
+                Console.WriteLine($"Deleting {tempFolder}");
+                Directory.Delete(tempFolder, true);
+                Console.WriteLine("Done!");
 
-		private static string ExtractTGZ(FileStream stream)
-		{
-			string uuid = Guid.NewGuid().ToString();
-			string outputDir = Path.Combine(UserTempFolder, uuid);
+                if(waitBeforeExit)
+                {
+                    Console.WriteLine("Press any key to close...");
+                    Console.ReadKey();
+                }
+            }
+        }
 
-			Stream gzipStream = new GZipInputStream(stream);
-			TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream, Encoding.ASCII);
+        private static void OutputHelp()
+        {
+            Console.WriteLine("Usage: \"extract.exe [unitypackage file] [options]\"");
+            Console.WriteLine("Use -nometa to skip output unity's meta files");
+            Console.WriteLine("Use -outputPreview to extract unity's preview files");
+            Console.WriteLine("Use -wait to let user press a key before exit the app");
+        }
 
-			tarArchive.ExtractContents(outputDir);
-			tarArchive.Close();
+        private static string GenreateOutputDirectory()
+        {
+            // must append a slash here to avoid 'space' ended filename
+            var filename = Path.GetFileNameWithoutExtension(UnitypackagePath);
+            var trimmed = filename.Trim();
+            if (trimmed != filename)
+                Console.WriteLine("[WARNING] The input package filename is different from the output folder because it begins/ends with spaces");
 
-			gzipStream.Close();
-			stream.Close();
+            return Path.Combine(Path.GetFullPath(Path.Combine(UnitypackagePath, @"..\")), trimmed);
+        }
 
-			return outputDir + Path.DirectorySeparatorChar;
-		}
+        private static void ExtractTGZ(string outputDir, FileStream stream)
+        {
+            Stream gzipStream = new GZipInputStream(stream);
+            TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream, Encoding.ASCII);
 
-		private static void ExtractFile(string folder, string output)
-		{
-			byte[] fileData = new byte[0];
-			string filePath = "";
-			byte[] fileMetaData = new byte[0];
-			byte[] filePreviewData = new byte[0];
+            tarArchive.ExtractContents(outputDir);
+            tarArchive.Close();
 
-			string filename = "";
+            gzipStream.Close();
+            stream.Close();
+        }
 
-			IEnumerable<string> files = Directory.EnumerateFiles(folder);
-			foreach (string file in files)
-			{
-				switch (Path.GetFileName(file))
-				{
-					case "asset":
-						fileData = File.ReadAllBytes(file);
-						break;
-					case "pathname":
-						filePath = File.ReadAllText(file);
-						filename = Path.GetFileName(filePath);
-						break;
-					case "asset.meta":
-						fileMetaData = File.ReadAllBytes(file);
-						break;
-					case "preview.png":
-						filePreviewData = File.ReadAllBytes(file);
-						break;
-					default:
-						Console.WriteLine($"Unexpected file: {file}");
-						break;
-				}
-			}
+        private static string ExtractFile(string folder)
+        {
+            byte[] fileData = null;
+            string filePath = "";
+            byte[] fileMetaData = null;
+            byte[] filePreviewData = null;
 
-			if (filePath == "")
-			{
-				Console.Error.WriteLine($"File path missing! {folder}");
-				return ;
-			}
+            IEnumerable<string> files = Directory.EnumerateFiles(folder);
+            foreach (string file in files)
+            {
+                switch (Path.GetFileName(file))
+                {
+                    case "asset":
+                        fileData = File.ReadAllBytes(file);
+                        break;
+                    case "pathname":
+                        filePath = File.ReadAllText(file).Split('\n')[0].Trim('\r', '\n', '\0');
+                        break;
+                    case "asset.meta":
+                        if (outputMetaFiles) fileMetaData = File.ReadAllBytes(file);
+                        break;
+                    case "preview.png":
+                        if (outputPreviewFiles) filePreviewData = File.ReadAllBytes(file);
+                        break;
+                    default:
+                        lock (global) { Console.WriteLine($"[WARNING] Unexpected file: {file}"); }
+                        break;
+                }
+            }
 
-			filePath = Path.Combine(OutputDirectory, filePath);
-			Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new FileNotFoundException($"File path missing in {folder}");
 
-			if (fileData.Length < 1)
-			{
-				// Theese are most likely 'assets' generated
-				// by unity that define directories
+            filePath = Path.Combine(OutputDirectory, filePath);
+            
+            var assetFolder = Path.GetDirectoryName(filePath);
+            lock (global)
+            {
+                try
+                {
+                    if (!Directory.Exists(assetFolder))
+                        Directory.CreateDirectory(assetFolder);
+                }
+                catch
+                {
+                    throw;
+                }
+            }
 
-				//Console.Error.WriteLine($"Asset is size 0 \"{folder}\"");
-				//File.WriteAllBytes(Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + ".emptyasset") , new byte[0]);
-			}
+            if (fileData != null && fileData.Length > 0)
+            {
+                File.WriteAllBytes(filePath, fileData);
+            }
+            else
+            {
+                // Theese are most likely 'assets' generated
+                // by unity that define directories
 
-			else
-			{
-				File.WriteAllBytes(filePath, fileData);
-			}
+                // lock (global) { Console.WriteLine($"[WARNING] Asset \"{filePath}\" is size 0 in \"{folder}\""); }
+                //File.WriteAllBytes(Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + ".emptyasset") , new byte[0]);
+            }
 
-			if(filePreviewData.Length > 0)
-			{
-				if (outputPreviewFiles) File.WriteAllBytes(Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "_preview.png"), filePreviewData);
-			}
+            if (filePreviewData != null && filePreviewData.Length > 0)
+            {
+                File.WriteAllBytes(Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "_preview.png"), filePreviewData);
+            }
 
-			if (fileMetaData.Length > 0)
-			{
-				if (outputMetaFiles) File.WriteAllBytes(filePath + ".meta", fileMetaData);
-			}
+            if (fileMetaData != null && fileMetaData.Length > 0)
+            {
+                File.WriteAllBytes(filePath + ".meta", fileMetaData);
+            }
 
-			Interlocked.Increment(ref ThreadWork);
-		}
+            return filePath;
+        }
 
-		private static string GetUnityPackage()
-		{
-			Console.WriteLine("Please drop a unitypackge on this window and press enter.");
-			var upp = Console.ReadLine();
-			if (!File.Exists(upp))
-			{
-				Console.WriteLine("Error");
-				Console.WriteLine(" ");
-				upp = GetUnityPackage();
-			}
+        private static string GetUnityPackage()
+        {
+            Console.WriteLine("Please drop a unitypackge on this window and press enter.");
+            var upp = Console.ReadLine();
+            upp = upp.Trim('"'); // handle the path with 'space' properly
 
-			if(Path.GetExtension(upp) != ".unitypackage")
-			{
-				Console.WriteLine("Error");
-				Console.WriteLine(" ");
-				upp = GetUnityPackage();
-			}
-			return upp;
-		}
+            if (!File.Exists(upp))
+            {
+                Console.WriteLine("Cannot find file: " + upp);
+                Console.WriteLine(" ");
+                upp = GetUnityPackage();
+            }
 
-	}
+            if (Path.GetExtension(upp) != ".unitypackage")
+            {
+                Console.WriteLine("Wrong Extension: " + Path.GetExtension(upp));
+                Console.WriteLine(" ");
+                upp = GetUnityPackage();
+            }
+            return upp;
+        }
+
+    }
 }
